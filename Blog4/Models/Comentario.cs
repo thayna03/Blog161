11de52cdc3b1ebc81b722a256fa56060bd01215a
thaynachagas53@gmail.com <thaynachagas53@gmail.com>
2019-08-08 16:10:18 -0400

    Inicio Projeto

---
+++ Blog4/Models/Comentario.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog4.Models
+{
+    public class Comentario
+    {
+        public int Id { get; set; }
+        [ForeignKey("MensagemId")]
+        public Mensagem mensagem { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataComentario { get; set; }
+        public string Autor { get; set; }
+    }
+}
---
+++ Blog4/Models/Mensagem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog4.Models
+{
+    public class Mensagem
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataDeCriacao { get; set; }
+        public string Categoria { get; set; }
+    }
+}
